333092eab2f0b5f80eda943c79c4a3e1599c737a
unknown <billy_lin00@yeah.net>
2025-05-03 21:45:40 +0800

    Update UI, dependencies, and configuration
    This commit includes several updates: added draggable title bar styling and functionality in `index.html`, updated Tauri dependencies in `package.json` and `package-lock.json`, modified Tauri configuration to enable window decorations, and added commented-out window control commands in `lib.rs`. Additionally, several image assets were updated.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,3 +12,33 @@ pub fn run() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+// use tauri::Manager; // 引入 Manager 以便访问窗口
+
+// #[tauri::command]
+// fn minimize_window(window: tauri::Window) {
+//     window.minimize().unwrap();
+// }
+
+// #[tauri::command]
+// fn maximize_window(window: tauri::Window) {
+//     window.maximize().unwrap();
+// }
+
+// #[tauri::command]
+// fn close_window(window: tauri::Window) {
+//     window.close().unwrap();
+// }
+
+// #[cfg_attr(mobile, tauri::mobile_entry_point)]
+// pub fn run() {
+//     tauri::Builder::default()
+//         .plugin(tauri_plugin_opener::init())
+//         .invoke_handler(tauri::generate_handler![
+//             minimize_window,
+//             maximize_window,
+//             close_window
+//         ])
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
